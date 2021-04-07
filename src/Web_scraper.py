b6179c66f1a68bd984a23e26290a4273e00d8bc9
Miguel A <80787912+miguel-a-ngel@users.noreply.github.com>
2021-04-08 00:10:32 +0200

    Añadido persistencia de datos en CSV y código comentado
    Añadido persistencia de datos en CSV y código comentado

--- src/Web_scraper.py
+++ src/Web_scraper.py
@@ -8,9 +8,13 @@ import unidecode
 
 #driver = webdriver.Firefox(executable_path='/Users/JRamon/Downloads/geckodriver')
 
+#driver de selenium
 driver = webdriver.Firefox(executable_path = '..\geckodriver.exe')
+#página web
 link_players = 'https://www.worldpadeltour.com/jugadores/'
 index = 'https://www.worldpadeltour.com'
+
+#Definimos cabecera distinta a la por defecto
 headers = {
 "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,\
 */*;q=0.8",
@@ -24,11 +28,13 @@ headers = {
 37.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36"
 }
 
-player_attributes_one = ["ranking", "puntos", "partidos_jugados","partidos_ganados","partidos_perdidos","efectividad","racha_victorias"]
+#Lista de atributos de jugadores
+player_attributes_one = ["name","ranking", "puntos", "partidos_jugados","partidos_ganados","partidos_perdidos","efectividad","racha_victorias"]
 player_attributes_two = ["compañero", "posicion", "lugar nacimiento", "fecha nacimiento", "altura", "residencia"]
 statistics_attributes = ["partidos jugados", "partidos ganados", "efectividad", "campeon", "finalista", "semifinalista", "cuartos", "octavos", "dieciseisavos"]
 years = ["2021", "2020", "2019", "2018", "2017", "2016", "2015", "2014", "2013"]
 
+#Función que elimina los acentos de un cadena de caracteres
 def remove_accents(raw_text):
     raw_text = re.sub(u"[àáâãäå]", 'a', raw_text)
     raw_text = re.sub(u"[èéêë]", 'e', raw_text)
@@ -40,32 +46,41 @@ def remove_accents(raw_text):
     raw_text = re.sub(u"[ñ]", 'n', raw_text)
     return raw_text
 
+
 def camel_case_split(name):
 	splitted = re.sub('([A-Z][a-z]+)', r' \1',re.sub('([A-Z]+)', r' \1', name)).split()
 	converted_list = [x.lower() for x in splitted]
 	return converted_list
 
+#Función que compone la url del jugador con '-'
 def compose_url(array_name):
 	new_url = '-'.join(array_name)
 	return remove_accents(new_url)
 
+#Función principal que construye la url final de cada jugador
 def build_url(name):
 	compound_url = link_players + compose_url(camel_case_split(name)) + '/'
 	return compound_url
 
 def get_attributes(url_player):
     web_player = requests.get(url_player)
+    player_list_one = []
+    player_list_two = []
     if(web_player.status_code == 200) :
         content_player = BeautifulSoup(web_player.content, "lxml")
-        i = 0
+        #Nombre del jugador
+        player_list_one.append(content_player.find('h1', class_='c-ranking-header__title').text)
+        i = 1
         for data in content_player.find_all('div', class_='c-ranking-header__data-box'):
             new_data = data.find('p', class_='c-ranking-header__data').text
             print(player_attributes_one[i] + " : "  + new_data)
+            player_list_one.append(new_data)
             i+=1
         j = 0
         for more_data in content_player.find_all('li', class_='c-player__data-item'):
             item = more_data.find('p').text
             print(player_attributes_two[j] + " : "  + item)
+            player_list_two.append(item)
             j+=1
         statistics_attributes_index = 0
         year_index = 0
@@ -79,13 +94,30 @@ def get_attributes(url_player):
             print(years[year_index] + " " + statistics_attributes[statistics_attributes_index] + " : " + statistics.text)
             statistics_attributes_index+=1
             count_statistics+=1
-            
-  
+    else:
+    	print('it doesnt connect')
+    return player_list_one + player_list_two
+
+#Procdimiento que persiste a un jugador en un fichero CSV
+def persist(player):
+	with open('statistics_players.csv', 'a', newline='') as csvfile:
+		storer = csv.writer(csvfile, delimiter='|', quotechar='.', quoting=csv.QUOTE_MINIMAL)
+		storer.writerow(player)
+
+#Procedimiento que prepara todo lo necesario para el procesamiento de un jugador:
+# Llama a las funciones que construyen la url.
+# A la que obtiene los atributos que queremos almacenar en el fichero csv.
+# A la que almacena la fila del jugador en el csv.
 def process_player(name):
 	url_player = build_url(name)
 	print("Procesando: ", url_player)
-	get_attributes(url_player)
+	player = get_attributes(url_player)
+	print(player)
+	persist(player)
 
+#Procedimiento principal que llama a todas las funciones definidas.
+# Realiza un scroll down con Selenium en la página de resumen 
+# donde se muestran los jugadores, para principalmente obtener todos los jugadores de Padel.
 def scroll_down(driver, link):
 	driver.get(link)
 	driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
@@ -97,17 +129,21 @@ def scroll_down(driver, link):
 	count = 1 #Player counter
 
 	for player in content.find_all('li', class_='c-player-card__item'):
+		#if count == 3:
+			#break
+
 		name = player.find('div', class_='c-player-card__name').text
 		score = player.find('div', class_='c-player-card__score').text
 		print("Jugador / puntuacion \n")
 		print(name + '\n' + score)
 		print(" ··········· \n")
 		process_player(name)
-		time.sleep(1)
+		time.sleep(10)
 		count += 1
 
 	print("Contador de jugadores ", count)
 	driver.close()
 
-#main
+#-------------------------------------------- main
+persist(player_attributes_one + player_attributes_two) #añadimos cabecera al csv
 scroll_down(driver, link_players)
