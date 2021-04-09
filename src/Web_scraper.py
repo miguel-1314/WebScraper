0537d8603930c21a42c87e845d00e438d7449c69
JRamonMC <joseramon.martinez888@gmail.com>
2021-04-09 17:07:12 +0200

    Comentarios añadidos

--- src/Web_scraper.py
+++ src/Web_scraper.py
@@ -6,11 +6,12 @@ import csv
 import re
 import unidecode
 
-#driver = webdriver.Firefox(executable_path='/Users/JRamon/Downloads/geckodriver')
+driver = webdriver.Firefox(executable_path='/Users/JRamon/Downloads/geckodriver')
 
-#driver de selenium
-driver = webdriver.Firefox(executable_path = '..\geckodriver.exe')
-#página web
+#Driver de selenium 
+#driver = webdriver.Firefox(executable_path = '..\geckodriver.exe')
+#Pagina web de World Padel Tour en la que nos aparece un listado con todos los jugadores, tanto
+#del ranking masculino como del ranking femenino
 link_players = 'https://www.worldpadeltour.com/jugadores/'
 index = 'https://www.worldpadeltour.com'
 
@@ -28,13 +29,15 @@ headers = {
 37.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36"
 }
 
-#Lista de atributos de jugadores
+#Lista de atributos de jugadores, divididos en 3 arrays distintos por simplicidad, dado que cada uno
+#de los arrays aparece en un punto determinado de la página
 player_attributes_one = ["name","ranking", "puntos", "partidos_jugados","partidos_ganados","partidos_perdidos","efectividad","racha_victorias"]
 player_attributes_two = ["compañero", "posicion", "lugar nacimiento", "fecha nacimiento", "altura", "residencia"]
 statistics_attributes = ["partidos jugados", "partidos ganados", "efectividad", "campeon", "finalista", "semifinalista", "cuartos", "octavos", "dieciseisavos"]
 years = ["2021", "2020", "2019", "2018", "2017", "2016", "2015", "2014", "2013"]
 
-#Función que elimina los acentos de un cadena de caracteres
+#Función que elimina los acentos de un cadena de caracteres, así como de convertir algunas 
+#cadenas en otras que nos interesen
 def remove_accents(raw_text):
     raw_text = re.sub(u"[àáâãäå]", 'a', raw_text)
     raw_text = re.sub(u"[èéêë]", 'e', raw_text)
@@ -46,18 +49,21 @@ def remove_accents(raw_text):
     raw_text = re.sub(u"[ñ]", 'n', raw_text)
     return raw_text
 
-
+#Método que separa la cadena de NombreApellido1Apellido2 en Nombre, Apellido 1 y Apellido 2
+# y que convierte a letra minuscula para la posterior construcción de la URL
 def camel_case_split(name):
 	splitted = re.sub('([A-Z][a-z]+)', r' \1',re.sub('([A-Z]+)', r' \1', name)).split()
 	converted_list = [x.lower() for x in splitted]
 	return converted_list
 
-#Función que compone la url del jugador con '-'
+#Función que compone la URL específica del recurso jugador con '-' para concordar con el formato de URL que utiliza
+#el sitio web
 def compose_url(array_name):
 	new_url = '-'.join(array_name)
 	return remove_accents(new_url)
 
-#Función principal que construye la url final de cada jugador
+#Función principal que construye la url final de cada jugador, añadiendo el prefijo de World Padel Tour
+#para todos los jugadores
 def build_url(name):
 	compound_url = link_players + compose_url(camel_case_split(name)) + '/'
 	return compound_url
